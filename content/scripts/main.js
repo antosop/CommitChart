50b6fcc4ad30eb6c60b76e368f7a90c508c83d53
Landon Crapo <antosop@gmail.com>
2015-05-08 01:13:38 -0600

    fixed errors but react-chartjs is broken... :P

--- content/scripts/main.js
+++ content/scripts/main.js
@@ -1,13 +1,13 @@
-var Doughnut = require('react-chartjs').Doughnut;
+var Doughnut = require('react-chartjs').Pie;
 var _ = require('lodash');
 var React = require('react');
 
 var Contribution = React.createClass({
     render: function() {
-        return <Doughnut data={this.state.data} />;
+        return <Doughnut data={this.state.data} width="500" height="500"/>;
     },
     getInitialState: function() {
-        setInterval(this.getData(), 5000);
+        setInterval(this.getData, 5000);
         return {
             colors: [
                 ['#FF0000', '#FF4444'],
@@ -15,20 +15,29 @@ var Contribution = React.createClass({
                 ['#00FF00', '#44FF44'],
                 ['#008888', '#44CCCC'],
                 ['#0000FF', '#4444FF'],
-                ['#880088', '#CC44CC']
+                ['#880088', '#CC44CC'],
+                ['#555555', '#888888'],
+                ['#FF0000', '#FF4444'],
+                ['#888800', '#CCCC44'],
+                ['#00FF00', '#44FF44'],
+                ['#008888', '#44CCCC'],
+                ['#0000FF', '#4444FF'],
+                ['#880088', '#CC44CC'],
+                ['#555555', '#888888']
             ],
             data: {}
         };
     },
     getData: function(){
-        $.getJSON('data', function(data){
-            this.setState({
+        var that = this;
+        jQuery.getJSON('data', function(data){
+            that.setState({
                 data: _.map(data, function(item, index){
                     return {
                         label: item.name,
                         value: item.count,
-                        color: this.state.colors[index][0],
-                        highlight: this.state.colors[index][1]
+                        color: that.state.colors[index][0],
+                        highlight: that.state.colors[index][1]
                     };
                 })
             });
